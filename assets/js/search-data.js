895debf710f428e038154e3bc10efb42ea344b91
Rafael-Silva-Oliveira <70116258+Rafael-Silva-Oliveira@users.noreply.github.com>
2025-12-12 18:33:18 +0000

    Deploying to gh-pages from @ Rafael-Silva-Oliveira/rafael-silva-oliveira.github.io@69e61d057dd79f397691163b4b208bd350c47a85 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,18 +58,7 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/books/";
               },
-            },{id: "post-a-post-with-jupyter-notebook",
-        
-          title: "a post with jupyter notebook",
-        
-        description: "an example of a blog post with jupyter notebook",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/jupyter-notebook/";
-          
-        },
-      },{id: "post-a-distill-style-blog-post",
+            },{id: "post-a-distill-style-blog-post",
         
           title: "a distill-style blog post",
         
