7a2935bd1584793a9e4013eed6c098e05495abf5
Rafael-Silva-Oliveira <70116258+Rafael-Silva-Oliveira@users.noreply.github.com>
2025-12-12 19:36:13 +0000

    Deploying to gh-pages from @ Rafael-Silva-Oliveira/rafael-silva-oliveira.github.io@67c9d9a8e10587a6d2bafa07b746531c5f95b75b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "",
@@ -58,18 +51,7 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/books/";
               },
-            },{id: "post-a-distill-style-blog-post",
-        
-          title: "a distill-style blog post",
-        
-        description: "an example of a distill-style blog post and main elements",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2021/distill/";
-          
-        },
-      },{id: "books-12-rules-for-life",
+            },{id: "books-12-rules-for-life",
           title: '12 rules for life',
           description: "",
           section: "Books",handler: () => {
