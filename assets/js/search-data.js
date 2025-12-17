5e31e0e0aae36f151f3aeea127c9258d04a1ed34
Rafael-Silva-Oliveira <70116258+Rafael-Silva-Oliveira@users.noreply.github.com>
2025-12-17 23:05:23 +0000

    Deploying to gh-pages from @ Rafael-Silva-Oliveira/rafael-silva-oliveira.github.io@5b26632287e69b54e9cf7d674b8e0537ef5e6d3c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -65,13 +65,6 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
